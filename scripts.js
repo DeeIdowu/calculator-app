5f04c2a1f735a5bb441ecb7d3264d5b1dc93cff3
Dennis Idowu <dennis_idowu@live.com>
2020-11-20 01:03:47 -0500

    added class constructor and passed a constructor

--- scripts.js
+++ scripts.js
@@ -1,3 +1,28 @@
+//How to store the numbers clicked:
+class Calculator{
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement,
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+
+    clear(){
+
+    }
+
+    delete(){
+
+    }
+
+    appendNumber(number){
+        //click no. and add to screen
+    }
+
+    chooseOperation(operation){
+        //selection of operation: +, *, / & -:
+
+    }
+}
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalButton = document.querySelector('[data-equals]')
@@ -5,3 +30,5 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+
