f46e2ae39433d680d119a7e0bf2e412063cd38e9
Dennis Idowu <dennis_idowu@live.com>
2020-11-20 02:10:28 -0500

    edited getDisplayNumber function

--- scripts.js
+++ scripts.js
@@ -64,10 +64,28 @@ class Calculator{
         this.previousOperand = ''
     }
 
+    getDisplayNumber(number){
+        //placing commas:
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if(isNaN(integerDigits)){
+            integerDisplay = ''
+        } else {
+            integetDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0})
+        }
+        if(decimalDigits != null){
+            return `${integerDisplay}.${decimalDigits}`
+        } else{
+            return integerDisplay
+        }
+    }
+
     updateDisplay(){
-        this.currentOperandTextElement.innerText = this.currentOperand
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
         if(this.operation != null){
-            this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`
+            this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
         }
     }
 }
