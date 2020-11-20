5c865aab9c36c880c11cc8e2c8b39adbd26cfa94
Dennis Idowu <dennis_idowu@live.com>
2020-11-20 01:20:52 -0500

    added operationsButton event listener for clicking operation buttons

--- scripts.js
+++ scripts.js
@@ -53,3 +53,8 @@ numberButtons.forEach(button => button.addEventListener('click', ()=>{
     calculator.appendNumber(button.innerText)
     calculator.updateDisplay()
 }))
+//event listener for click of operation buttons:
+operationButtons.forEach(button => button.addEventListener('click', ()=>{
+    calculator.chooseOperation(button.innerText)
+    calculator.updateDisplay()
+}))
